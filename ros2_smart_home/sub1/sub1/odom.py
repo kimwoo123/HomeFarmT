e9b4320384f7165a909a1c754bfa4b9e2fe02e55
김담영 <damyoung1225@gmail.com>
2021-09-01 20:09:50 +0900

    <ROS/dam> ADD : IMU를 이용한 헤딩 구하기 터틀봇 상태를 이용한 헤딩 구하는 것도 구현

--- ros2_smart_home/sub1/sub1/odom.py
+++ ros2_smart_home/sub1/sub1/odom.py
@@ -69,7 +69,21 @@ class odom(Node):
         self.laser_transform.transform.rotation.w = 
 
         '''
-                      
+
+        self.odom_msg.header.frame_id = 'map'
+        self.odom_msg.child_frame_id = 'base_link'
+
+        self.base_link_transform.header.frame_id = 'map'
+        self.base_link_transform.child_frame_id = 'base_link'
+
+        self.laser_transform.header.frame_id = 'base_link'
+        self.laser_transform.child_frame_id = 'laser'
+        self.laser_transform.transform.translation.x = 0.0
+        self.laser_transform.transform.translation.y = 0.0
+        self.laser_transform.transform.translation.z = 1.0
+        self.laser_transform.transform.rotation.w = 1.0
+
+
 
     def imu_callback(self,msg):
         pass
@@ -86,10 +100,22 @@ class odom(Node):
             self.theta=
 
         '''
+        if self.is_imu ==False :    
+            self.is_imu=True
+            imu_q = Quaternion(msg.orientation.w, msg.orientation.x, msg.orientation.y, msg.orientation.z)
+            print('imu_q')
+            print(imu_q)
+            self.imu_offset = -imu_q.to_euler()[2]
+            print('imu_offset')
+            print(self.imu_offset)
+
+        else :
+            imu_q = Quaternion(msg.orientation.w, msg.orientation.x, msg.orientation.y, msg.orientation.z)
+            self.theta = imu_q.to_euler()[2] + self.imu_offset
 
 
     def listener_callback(self, msg):
-        print('linear_vel : {}  angular_vel : {}'.format(msg.twist.linear.x,-msg.twist.angular.z))
+        # print('linear_vel : {}  angular_vel : {}'.format(msg.twist.linear.x,-msg.twist.angular.z))
         if self.is_imu ==True:
             if self.is_status == False :
                 self.is_status=True
@@ -113,6 +139,9 @@ class odom(Node):
                 self.theta+=
 
                 '''
+                self.x += linear_x * cos(self.theta) * self.period
+                self.y +=  linear_x * sin(self.theta) * self.period
+                # self.theta += angular_z * self.period
 
                 self.base_link_transform.header.stamp =rclpy.clock.Clock().now().to_msg()
                 self.laser_transform.header.stamp =rclpy.clock.Clock().now().to_msg()
@@ -140,10 +169,28 @@ class odom(Node):
 
                 '''
 
+                q = Quaternion.from_euler(0, 0, self.theta)
+                
+                self.base_link_transform.transform.translation.x = self.x
+                self.base_link_transform.transform.translation.y = self.y
+                self.base_link_transform.transform.rotation.x = q.x
+                self.base_link_transform.transform.rotation.y = q.y
+                self.base_link_transform.transform.rotation.z = q.z
+                self.base_link_transform.transform.rotation.w = q.w
+                
+                self.odom_msg.pose.pose.position.x = self.x
+                self.odom_msg.pose.pose.position.y = self.y
+                self.odom_msg.pose.pose.orientation.x = q.x
+                self.odom_msg.pose.pose.orientation.y = q.y
+                self.odom_msg.pose.pose.orientation.z = q.z
+                self.odom_msg.pose.pose.orientation.w = q.w
+                self.odom_msg.twist.twist.linear.x = linear_x
+                self.odom_msg.twist.twist.angular.z = angular_z
+
                 self.broadcaster.sendTransform(self.base_link_transform)
                 self.broadcaster.sendTransform(self.laser_transform)
                 self.odom_publisher.publish(self.odom_msg)
-                self.prev_time=self.current_time
+                self.prev_time = self.current_time
 
         
 def main(args=None):
