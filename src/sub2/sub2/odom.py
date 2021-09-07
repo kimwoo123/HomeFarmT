ce2688ab5a66a9004d065177d2b146676a20d267
dongyun <tlsdh1111@gmail.com>
2021-09-07 15:48:19 +0900

    [AD/dong] FIX | 스마트홈 1 맵에 대한 좌표 수정

--- src/sub2/sub2/odom.py
+++ src/sub2/sub2/odom.py
@@ -40,65 +40,52 @@ class odom(Node):
         self.is_calc_theta=False
         # x,y,theta는 추정한 로봇의 위치를 저장할 변수 입니다.        
         # 로봇의 초기위치를 맵 상에서 로봇의 위치와 맞춰줘야 합니다. 
-        self.x=0
-        self.y=0
-        self.theta=0.0
+        self.x = 0
+        self.y = 0
+        self.theta = 0.0
         # imu_offset은 초기 로봇의 orientation을 저장할 변수 입니다.
-        self.imu_offset=0
-        self.prev_time=0
+        self.imu_offset = 0
+        self.prev_time = 0
 
-        
-        '''
-        로직 2. publish, broadcast 할 메시지 설정
-        self.odom_msg.header.frame_id=
-        self.odom_msg.child_frame_id=
+        self.odom_msg.header.frame_id = 'map'
+        self.odom_msg.child_frame_id = 'base_link'
 
-        self.base_link_transform.header.frame_id = 
-        self.base_link_transform.child_frame_id = 
+        self.base_link_transform.header.frame_id = 'map' 
+        self.base_link_transform.child_frame_id = 'base_link'
 
-        self.laser_transform.header.frame_id = 
-        self.laser_transform.child_frame_id =      
-        self.laser_transform.transform.translation.x = 
-        self.laser_transform.transform.translation.y = 
-        self.laser_transform.transform.translation.z = 
-        self.laser_transform.transform.rotation.w = 
-        '''
-    
+        self.laser_transform.header.frame_id = 'base_link'
+        self.laser_transform.child_frame_id = 'laser'     
+        self.laser_transform.transform.translation.x = 0.0 
+        self.laser_transform.transform.translation.y = 0.0
+        self.laser_transform.transform.translation.z = 1.0 
+        self.laser_transform.transform.rotation.w = 1.0
 
     def listener_callback(self, msg):
         print('linear_vel : {}  angular_vel : {}'.format(msg.twist.linear.x,-msg.twist.angular.z))        
         if self.is_status == False :
-            self.is_status=True
+            self.is_status = True
             self.prev_time=rclpy.clock.Clock().now()
         else :
             
-            self.current_time=rclpy.clock.Clock().now()
+            self.current_time = rclpy.clock.Clock().now()
             # 계산 주기를 저장한 변수 입니다. 단위는 초(s)
-            self.period=(self.current_time-self.prev_time).nanoseconds/1000000000
+            self.period = (self.current_time - self.prev_time).nanoseconds / 1000000000
             # 로봇의 선속도, 각속도를 저장하는 변수, 시뮬레이터에서 주는 각 속도는 방향이 반대이므로 (-)를 붙여줍니다.
-            linear_x=msg.twist.linear.x
-            angular_z=-msg.twist.angular.z
-            '''
-            로직 3. 로봇 위치 추정
-            (테스트) linear_x = 1, self.theta = 1.5707(rad), self.period = 1 일 때
-            self.x=0, self.y=1 이 나와야 합니다. 로봇의 헤딩이 90도 돌아가 있는
-            상태에서 선속도를 가진다는 것은 x축방향이 아니라 y축방향으로 이동한다는 뜻입니다. 
-            #절대위치 사용
-            self.x+=
-            self.y+=
-            self.theta+=
-            '''              
+            linear_x = msg.twist.linear.x
+            angular_z = -msg.twist.angular.z
+   
+            self.x += linear_x * cos(self.theta) * self.period
+            self.y += linear_x * sin(self.theta) * self.period
+            self.theta += angular_z * self.period  
+            
             self.base_link_transform.header.stamp =rclpy.clock.Clock().now().to_msg()
             self.laser_transform.header.stamp =rclpy.clock.Clock().now().to_msg()
             
-            '''
-            로직 4. 추정한 로봇 위치를 메시지에 담아 publish, broadcast
-            '''
             q = Quaternion.from_euler(0, 0, self.theta)
             
             self.base_link_transform.transform.translation.x = self.x
             self.base_link_transform.transform.translation.y = self.y
-            self.base_link_transform.transform.rotation.x = q.x
+            self.base_link_transform.transform.rotation.x = q.x 
             self.base_link_transform.transform.rotation.y = q.y
             self.base_link_transform.transform.rotation.z = q.z
             self.base_link_transform.transform.rotation.w = q.w
@@ -134,9 +121,3 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
-
-
-
-
-       
-   
