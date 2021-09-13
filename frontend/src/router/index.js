ca3d7760f0495ca82edb70649d2ca6639bd3ada3
daeun <zerozero7bang@gmail.com>
2021-09-13 17:47:14 +0900

    [FE/eun] ADD : 뷰, 컴포넌트 구조화

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Signup from '../views/Signup.vue'
-import Login from '../views/Login.vue'
+import Signup from '../views/user/Signup.vue'
+import Login from '../views/user/Login.vue'
 import Home from '../views/Home.vue'
 import IoT from '../views/IoT.vue'
+import Schedule from '../views/Schedule.vue'
+import Patrol from '../views/Patrol.vue'
+import NavbarLayout from '../views/NavbarLayout.vue'
 
 Vue.use(VueRouter)
 
@@ -24,9 +27,25 @@ const routes = [
     component: Home
   },
   {
-    path: '/iot',
-    name: 'IoT',
-    component: IoT
+    path: '/navbar-layout',
+    component: NavbarLayout,
+    children: [
+      {
+        path: '/iot',
+        name: 'IoT',
+        component: IoT
+      },
+      {
+        path: '/schedule',
+        name: 'Schedule',
+        component: Schedule
+      },
+      {
+        path: '/patrol',
+        name: 'Patrol',
+        component: Patrol
+      },
+    ]
   }
 ]
 
