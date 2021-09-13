b5e5c8690b6101aa13e3e4bc6a0208b52d756d24
daeun <zerozero7bang@gmail.com>
2021-09-14 05:55:18 +0900

    [FE/eun] UPDATE : nav, 텍스트 정렬 css 삭제

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -19,23 +19,9 @@
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
     background-color: white;
   }
   body {
     background-color: grey;
-  }
-  #nav {
-    padding: 30px;
-  }
-
-  #nav a {
-    font-weight: bold;
-    color: #2c3e50;
-  }
-
-  #nav a.router-link-exact-active {
-    color: #42b983;
   }
 </style>
