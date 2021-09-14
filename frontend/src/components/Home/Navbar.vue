b8262952d09b03378d258d438906b4b57a65e28e
daeun <zerozero7bang@gmail.com>
2021-09-14 14:29:15 +0900

    [FE/eun] ADD : home navbar

---
+++ frontend/src/components/Home/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="nav-container">
+    <div class="nav-text-container">
+      <small>with turtlebot</small>
+      <div>
+        <span>Make</span><span class="point-color">Your Farm</span>
+      </div>
+    </div>
+    <font-awesome-icon icon="user" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @import '@/components/Home/Navbar.scss';
+</style>
+
+<script>
+export default {  
+  name: 'Navbar',
+}
+</script>
+
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home-conatiner">
+    <Navbar/>
     <HomeBotStatus/>
   </div>
 </template>
@@ -7,10 +8,12 @@
   @import url('./Home.scss');
 </style>
 <script>
+import Navbar from '../components/Home/Navbar.vue'
 import HomeBotStatus from '../components/Home/HomeBotStatus.vue'
 export default {
   name: "Home",
   components: {
+    Navbar,
     HomeBotStatus,
   }
 
