78fc662cdaf67e86d4db3d58cad6e25279cd2068
dongyun <tlsdh1111@gmail.com>
2021-09-15 14:35:29 +0900

    [FE/dong] ADD : Patrol Component Completed

--- frontend/src/views/Patrol.vue
+++ frontend/src/views/Patrol.vue
@@ -1,16 +1,23 @@
 <template>
-  <div>
-Patrol
+  <div class="patrol-container">
+    <PatrolCam/>
+    <PatrolMenu/>
   </div>
 </template>
-
+<style>
+  @import './Patrol.scss';
+</style>
 <script>
+import PatrolCam from '../components/Patrol/PatrolCam.vue'
+import PatrolMenu from '../components/Patrol/PatrolMenu.vue'
+
 export default {
   name: 'Patrol',
+  components: {
+    PatrolCam,
+    PatrolMenu,
+  }
 
 }
 </script>
 
-<style>
-
-</style>
