b0638c9a8b4ace714e6fe6b46b29c13a4e1e9d20
daeun <zerozero7bang@gmail.com>
2021-09-24 11:16:33 +0900

    [FE/eun] UPDATE : test페이지 소켓연결

--- frontend/src/views/Test.vue
+++ frontend/src/views/Test.vue
@@ -1,12 +1,83 @@
 <template>
   <div>
-    Test
+    <form>
+      <!-- 로직 1. textarea 생성 -->
+      <p>Security Status</p>
+      <textarea id = "tSafetyStatus" cols = 30 rows = 2></textarea>
+
+      <br>
+
+      <!-- 로직 2. 침입자 인지 시점 이미지 뷰어 생성 -->
+      <!-- <img src="cam.jpg?t=" width='480' onload='setTimeout(function() {src = src.substring(0, (src.lastIndexOf("t=")+2))+(new Date()).getTime()}, 50)'
+      onerror='setTimeout(function() {src = src.substring(0, (src.lastIndexOf("t=")+2))+(new Date()).getTime()}, 50)'/> -->
+
+      
+      <!-- 로직 3. 순찰 모드 스위치 버튼 생성 -->
+      <p>Patrol On/Off Status</p>
+      <textarea id = "tPatrolStatus" cols = 30 rows = 2></textarea>
+      <br>
+      <button type ='button' id="A1-btn" @click="btn_patrol_on">On</button>
+      <button type ='button' id="A2-btn" @click="btn_patrol_off">Off</button>
+      
+      <br>
+      <!-- 로직 4. 수동 컨트롤 버튼 생성 -->
+      <p>Manual Controller</p>
+      <button type ='button' id="A1-btn" @click="btn_turn_left">Turn Left</button>
+      <button type ='button' id="A2-btn" @click="btn_go_straight">Go Straight</button>
+      <button type ='button' id="A2-btn" @click="btn_turn_right">Turn Right</button>
+
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Test'
+  name: 'Test',
+  created() {
+
+    this.$socket.on('disconnect', function()  {
+        console.log('disconnected form server_client.');
+    });
+
+
+    // 로직 1. 서버에서 온 메시지를 웹페이지에 전달
+    this.$socket.on('sendSafetyStatus', function(message) {
+        console.log('sendSafetyStatus', message);
+        document.querySelector('#tSafetyStatus').value = message;
+    });
+
+    this.$socket.on('sendPatrolStatus', function(message) {
+        console.log('sendPatrolStatus', message);
+        document.querySelector('#tPatrolStatus').value = message;
+    });
+  },
+  methods: {
+    btn_patrol_on() {
+      console.log('btn_patrol_on');
+      let data = 1;
+      this.$socket.emit('PatrolOnToServer', data);
+    },
+    btn_patrol_off() {
+      console.log('btn_patrol_off');
+      let data = 0;
+      this.$socket.emit('PatrolOffToServer', data);
+    },
+    btn_turn_left() {
+      console.log('btn_left');
+      let data = 1;
+      this.$socket.emit('turnleftToServer', data);
+    },
+    btn_go_straight() {
+      console.log('btn_go_straight');
+      let data = 2;
+      this.$socket.emit('gostraightToServer', data);
+    },
+    btn_turn_right() {
+      console.log('btn_turn_right');
+      let data = 3;
+      this.$socket.emit('turnrightToServer', data);
+    },
+  }
 }
 </script>
 
