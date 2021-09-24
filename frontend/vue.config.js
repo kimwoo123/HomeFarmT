2c306926a123e866a702b1847489541c0dc80b6f
daeun <zerozero7bang@gmail.com>
2021-09-24 11:02:13 +0900

    [FE/eun] FIX : sass 오류

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,12 +2,10 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        sassOptions: {
-          additionalData: `
-            @import "@/assets/scss/normalize.scss";
-            @import "@/assets/scss/common.scss";
-          `
-        }
+        additionalData: `
+          @import "@/assets/scss/normalize.scss";
+          @import "@/assets/scss/common.scss";
+        `
       }
     }
   }
