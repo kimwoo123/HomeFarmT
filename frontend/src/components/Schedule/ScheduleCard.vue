6d6bd82207fbeb7f44a7e2febc159e8ab40dbedc
daeun <zerozero7bang@gmail.com>
2021-10-05 00:26:16 +0900

    [FE/eun] ADD : 스케줄 카드

---
+++ frontend/src/components/Schedule/ScheduleCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card-box shadows">
+
+    <div class="content-box">
+      <div class="content">
+        <span class="title">{{ title }}</span>
+        <span class="description">{{ description }}</span>
+      </div>
+      <img src="@/assets/icon/switch.svg" alt="">
+    </div>
+
+    <div class="time-box">
+      <img src="@/assets/icon/time.svg" alt="time" class="icon">
+      <span class="time">{{ time }}</span>
+      <img src="@/assets/icon/calender.svg" alt="date" class="icon">
+      <span class="date">{{ date }}</span>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScheduleCard',
+  data() {
+    return {
+      title: '감시하기',
+      description: '1번 경로로 감시를 실행합니다',
+      time: '오전 9:00',
+      date: [
+        '월', '화', '수',
+      ],
+      status: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '@/components/Schedule/ScheduleCard.scss';
+</style>
--- frontend/src/views/Schedule.vue
+++ frontend/src/views/Schedule.vue
@@ -1,24 +1,31 @@
 <template>
   <div>
+    <Navbar :title="'스케줄'" :left_icon="true" :right_text="'추가'"/>
+    <div class="schedule-container">
+      <ScheduleCard/>
+    </div>
     <ScheduleCalendar/>
     <!-- <SchedulePlan/> -->
   </div>
 </template>
 
-<style lang="scss" scoped>
-  @import '@/views/Schedule.scss';
-</style>
-
 <script>
+import Navbar from '@/components/common/Navbar.vue'
 import ScheduleCalendar from '../components/Schedule/ScheduleCalendar.vue'
 // import SchedulePlan from '../components/Schedule/SchedulePlan.vue'
-
+import ScheduleCard from '../components/Schedule/ScheduleCard.vue'
 
 export default {
   name: 'Schedule',
   components: {
+    Navbar,
     ScheduleCalendar,
-    // SchedulePlan
+    // SchedulePlan,
+    ScheduleCard
   },
 }
 </script>
+
+<style lang="scss" scoped>
+  @import '@/views/Schedule.scss';
+</style>
