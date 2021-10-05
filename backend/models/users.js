2f3e508f3a15ad985b91a9d58b403a1cdee3a202
김우석 <eypk9673@naver.com>
2021-10-05 11:37:20 +0900

    [BE/woo] ADD: historyAPI 통신을 위한 백엔드 구현

--- backend/models/users.js
+++ backend/models/users.js
@@ -3,7 +3,12 @@ sequelize.define('users', {
   email: { type: DataTypes.STRING, unique: true },
   userid: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
   hashid: { type: DataTypes.STRING },
-  password: { type: DataTypes.STRING, defaultValue: 'none' }
+  password: { type: DataTypes.STRING, defaultValue: 'none' },
+  route1: { type: DataTypes.STRING },
+  route2: { type: DataTypes.STRING },
+  route3: { type: DataTypes.STRING },
+  region: { type: DataTypes.STRING },
+  turtlebot: { type: DataTypes.STRING },
 }, {
   timestamps: false
 });
