301c185cb0852d27ff0b764c97867fc5a0bc6dfe
daeun <zerozero7bang@gmail.com>
2021-10-06 15:02:32 +0900

    [FE/eun] ADD : home 메뉴 hover 추가

--- frontend/src/components/Home/HomeMenus.vue
+++ frontend/src/components/Home/HomeMenus.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="menus">
-    <div class="shadows menu" @click="$router.push({ name: 'IoT' })">
-      <img src="@/assets/icons/iot.svg" alt="iot">
-      <span>제어</span>
+    <div class="shadows menu" @click="$router.push({ name: 'IoT' })" @mouseover="activeImg('iot')" @mouseout="originImg('iot')">
+      <img src="@/assets/icons/menus/iot.svg" alt="iot" id="iot">
+      <span>기기 제어</span>
     </div>
-    <div class="shadows menu" @click="$router.push({ name: 'Patrol' })">
-      <img src="@/assets/icons/patrol.svg" alt="patrol">
+    <div class="shadows menu" @click="$router.push({ name: 'Patrol' })" @mouseover="activeImg('patrol')" @mouseout="originImg('patrol')">
+      <img src="@/assets/icons/menus/patrol.svg" alt="patrol" id="patrol">
       <span>감시</span>  
     </div>
-    <div class="shadows menu" @click="$router.push({ name: 'Schedule' })">
-      <img src="@/assets/icons/schedule.svg" alt="schedule">
+    <div class="shadows menu" @click="$router.push({ name: 'Schedule' })" @mouseover="activeImg('schedule')" @mouseout="originImg('schedule')">
+      <img src="@/assets/icons/menus/schedule.svg" alt="schedule" id="schedule">
       <span>일정</span>  
     </div>
-    <div class="shadows menu" @click="$router.push({ name: 'Control' })">
-      <img src="@/assets/icons/control.svg" alt="control">
-      <span>조작</span>  
+    <div class="shadows menu" @click="$router.push({ name: 'Control' })" @mouseover="activeImg('control')" @mouseout="originImg('control')"> 
+      <img src="@/assets/icons/menus/control.svg" alt="control" id="control">
+      <span>터틀봇 조작</span>  
     </div>
   </div>
 </template>
@@ -22,6 +22,16 @@
 <script>
 export default {
   name: 'HomeMenus',
+  methods: {
+    activeImg(data) {
+      let target = document.querySelector(`#${data}`)
+      target.src = require(`@/assets/icons/menus/${data}_active.svg`)
+    },
+    originImg(data) {
+      let target = document.querySelector(`#${data}`)
+      target.src = require(`@/assets/icons/menus/${data}.svg`)
+    },
+  }
 }
 </script>
 
