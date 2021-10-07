f3dab0f0ce4a688c2a511f2e225142ed29c016ba
daeun <zerozero7bang@gmail.com>
2021-10-07 10:22:29 +0900

    [FE/eun] UPDATE : 스케줄 스위치 버튼

--- frontend/src/components/Schedule/ScheduleCard.vue
+++ frontend/src/components/Schedule/ScheduleCard.vue
@@ -6,30 +6,33 @@
         <span class="title">{{ title }}</span>
         <span class="description font-300">{{ description }}</span>
       </div>
-      <img src="@/assets/icons/switch.svg" alt="">
+      <SwitchBtn class="switch-align"/>
     </div>
 
     <div class="time-box">
-      <img src="@/assets/icons/time.svg" alt="time" class="icon">
-      <span class="time">{{ time }}</span>
       <img src="@/assets/icons/calender.svg" alt="date" class="icon">
       <span class="date">{{ date }}</span>
+      <img src="@/assets/icons/time.svg" alt="time" class="icon">
+      <span class="time">{{ time }}</span>
     </div>
 
   </div>
 </template>
 
 <script>
+import SwitchBtn from '@/components/common/SwitchBtn.vue'
+
 export default {
   name: 'ScheduleCard',
+  components: {
+    SwitchBtn
+  },
   data() {
     return {
       title: '감시하기',
       description: '1번 경로로 감시를 실행합니다',
       time: '오전 9:00',
-      date: [
-        '월', '화', '수',
-      ],
+      date: '2021년 10월 7일',
       status: true
     }
   }
